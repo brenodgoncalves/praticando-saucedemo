6df2331f7d0c8c4fef79255f822ffba61f89f074
Breno Goncalves <brenodgoncalves@gmail.com>
2025-09-22 18:11:26 -0300

    reutilizando massa de dados para todos os testes

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('deve logar com sucesso', async ({ page }) => {
-
-    const user = {
+const user = {
         username: 'standard_user',
         password: 'secret_sauce'
     };
+
+test('deve logar com sucesso', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page
@@ -21,5 +21,41 @@ test('deve logar com sucesso', async ({ page }) => {
     await expect(page
         .getByText('Products'))
         .toBeVisible();
+
+});
+
+test('não deve logar com senha incorreta', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+    await page
+        .getByPlaceholder('Username')
+        .fill(user.username);
+    await page
+        .getByPlaceholder('Password')
+        .fill(user.password+'incorreta');
+    await page
+        .getByRole('button', { name: 'Login' })
+        .click();
+    await expect(page
+        .locator('[data-test="error"]'))
+        .toHaveText("Epic sadface: Username and password do not match any user in this service");
+
+});
+
+test('não deve logar com usuário não cadastrado', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+    await page
+        .getByPlaceholder('Username')
+        .fill(user.username+'naoexiste');
+    await page
+        .getByPlaceholder('Password')
+        .fill(user.password);
+    await page
+        .getByRole('button', { name: 'Login' })
+        .click();
+    await expect(page
+        .locator('[data-test="error"]'))
+        .toHaveText("Epic sadface: Username and password do not match any user in this service");
 
 });
