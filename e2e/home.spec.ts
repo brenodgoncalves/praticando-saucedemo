640cc9b83eebba84dbe42d844eda8d47441ff0af
Breno Goncalves <brenodgoncalves@gmail.com>
2025-09-23 16:02:41 -0300

    melhorando cobertura na tela home

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,8 +1,25 @@
 import { test, expect } from '@playwright/test';
+import { getLoginPage } from '../support/pages/loginPage';
 
 test('has title', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
+  const loginPage = getLoginPage(page);
+
+  await loginPage.open();
   await expect(page).toHaveTitle(/Swag Labs/);
+
+});
+
+test('should have login button', async ({ page }) => {
+
+  const loginPage = getLoginPage(page);
+
+  await loginPage.open();
+  const labelUsername = page.getByPlaceholder('Username');
+  await expect(labelUsername).toBeVisible();
+  const labelPassword = page.getByPlaceholder('Password');
+  await expect(labelPassword).toBeVisible();
+  const loginButton = page.getByRole('button', { name: 'Login' });
+  await expect(loginButton).toBeVisible();
 
 });
